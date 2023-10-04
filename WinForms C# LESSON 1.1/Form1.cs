53f4fa6265a76d2648dd36c000477ced324356fd
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2023-10-04 23:02:45 +0400

    WinForms1.1
    WinForms1.1

---
+++ WinForms C# LESSON 1.1/Form1.cs
@@ -0,0 +1,75 @@
+﻿namespace WinForms_C__LESSON_1._1
+{
+    public partial class Form1 : Form
+    {
+
+        private Point startPoint;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            this.MouseDown += Form1_MouseDown;
+
+            this.MouseUp += From1_MouseUp;
+
+            
+        }
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                startPoint = e.Location;
+            }
+
+
+        }
+
+        private void From1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && startPoint != Point.Empty)
+            {
+               
+
+
+                if (Math.Abs(startPoint.X - e.X) > 9 && Math.Abs(startPoint.Y - e.Y)>9) {
+                    Label dynamicLabel;
+
+                    dynamicLabel = new Label();
+                    dynamicLabel.BorderStyle = BorderStyle.FixedSingle;
+
+                    dynamicLabel.Click += (object sender, EventArgs e) =>
+                    {
+                        dynamicLabel.Text = Random.Shared.Next(1000).ToString();
+                    };
+
+                    dynamicLabel.DoubleClick += (object sender, EventArgs e) =>
+                    {
+                        dynamicLabel.Dispose();
+                    };
+
+
+                    dynamicLabel.Font = new Font("Arial", 12);
+
+
+
+
+                    dynamicLabel.Location = new Point(Math.Min(startPoint.X, e.X), Math.Min(startPoint.Y, e.Y));
+                    dynamicLabel.Size = new Size(Math.Abs(startPoint.X - e.X), Math.Abs(startPoint.Y - e.Y));
+                    this.Controls.Add(dynamicLabel);
+                }
+            }
+
+        }
+
+
+       
+
+    }
+}
